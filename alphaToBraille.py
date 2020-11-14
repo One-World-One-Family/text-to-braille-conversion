e43a7deed37f63497d6ede8fecffa35a24eb8fe3
vishrutkohli <kohlivishrut@gmail.com>
2020-11-14 18:53:14 +0530

    refining documentation

--- alphaToBraille.py
+++ alphaToBraille.py
@@ -146,38 +146,3 @@ def translate(string):
         shavings = untrimmed_word.replace(trimmed_word, "")
         braille = build_braille_word(trimmed_word, shavings, index, braille) + " "
     return braille[:-1]  # Remove the final space that was added.
-
-'''
-The Algorithm for Translating Alphabet Based Text to Grade 2 Braille:
-1. Split up the text into words by dividing them based on whitespace characters.
-    - Whitespace includes spaces (' ') and new lines ('\n')
-2. For each word, handle the numbers first.
-    - Numbers in braille use the same symbols as the first 10 letters of the alphabet.
-        - The number '7' and the letter 'g' are both represented by '⠛'.
-        - To differentiate between numbers and letters, an escape code (⠼) is placed before groups of numbers.
-        - Therefore '7' is actually '⠼⠛' whereas 'g' is only '⠛'.
-    - In this step, only the numbers are dealt with, so there will be a mix of both braille and Alphabet symbols.
-        - Example: "123-456-JUNK" becomes "⠼⠁⠃⠉-⠼⠙⠑⠋-JUNK"
-3. Handle the capitals.
-    - Similarly to numbers in braille, capital letters need an escape code (⠠).
-    - The escape code (⠠) is added to the beginning of each capital letter and the letter is changed to lowercase.
-        - Example 1: "⠼⠁⠃⠉-⠼⠙⠑⠋-JUNK" becomes "⠼⠁⠃⠉-⠼⠙⠑⠋-⠠j⠠u⠠n⠠k". The dashes still remain.
-        - Example 2: "Sweet" becomes "⠠sweet". The non-capital letters remain untouched.
-4. Trim the word.
-    - Sometimes the words extracted contain punctuation attached to them such as commas or brackets.
-    - Words need to be trimmed so that they can be converted to contractions.
-        - Example: The word "the" is represented by a single braille symbol (⠮).
-        - If the word "the" has punctuation around it ("the!") then it will not be interpreted correctly.
-        - This is also why capitals are converted to lowercase in step 3 because "The" would not work either.
-    - The characters that are trimmed off are called "shavings".
-        - Example: In the word "!where?", the shavings are "!?" and the trimmed word is "where".
-5. Build the translation.
-    a) Check to see if the trimmed word can be contracted.
-        - This includes common words like "the", "in", "you" etc...
-    b) Translate the remaining characters that are still alphabetic.
-    c) Translate the shavings (this will mostly just be punctuation).
-        - Exceptions to be mindful of:
-            - There is no braille symbol for a generic quote (")
-            - There is only open quotation (“) and closed quotation (”).
-            - Therefore we must keep track of what the last quotation was to convert it correctly.
-'''
